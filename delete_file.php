d54d062b0aa1241cf0403e132ad548e680394dfd
AminMohammadii <86127676+AminMohammadii@users.noreply.github.com>
2022-09-04 20:02:46 +0430

    Improvement for delete_file.php

--- delete_file.php
+++ delete_file.php
@@ -1,53 +1,42 @@
 <?php 
+require_once("dbConnect.php");
+
+$server = SERVER_IP;
+$directory = FILES_DIR;
 
 // this function try to delete file from the category.
-function delete_file($path, $file_name, $info=""){
-
+function delete_file($file_name, $info=""){
+    global $server,$directory;
     // check file_name is empty or not
     if(! ($file_name === "" or $file_name == null) ){
 
-        $full_path = $path . "/" . $file_name; // ex : movies/سرشانه.png
+        $relDirectory = str_ireplace("/******", "../../..", $directory);
 
+        $full_path = $relDirectory . $file_name;
 
         // check if file exist in directory or not
         if(file_exists($full_path)){
     
             // try to delete file
             if(unlink($full_path)){
-                // echo json_encode(array("message" => "$info file deleted successfully" ,
-                                                // "status" => true)) ;  
                 return array("deleteMessage" => "$info file deleted successfully",
                 "deleteStatus" => true);                  
                   
             }else{
-                // $error_msg = json_encode(array("message" => "file did not delete!" ,
-                //                                 "status" => false)) ;  
-                // echo $error_msg ;
                 return array("deleteMessage" => "file did not delete!",
                 "deleteStatus" => false);
             }
         }
         else {
-            // $error_msg = json_encode(array("message" => "file '$file_name' did not found in '$path' directory" ,
-            //                                 "status" => false)) ;
-            // echo $error_msg ;
-            return array("deleteMessage" => "file '$file_name' did not found in directory",
+            return array("deleteMessage" => "could not find file '$file_name' in directory to delete",
             "deleteStatus" => false);
         }
     }
     else {
-        // $error_msg = json_encode(array("message" => "file name did not found, so no file has deleted" ,
-        //                                     "status" => false)) ;
-        //     echo $error_msg ;
-            return array("deleteMessage" => "file name did not found, so no file has deleted",
+            return array("deleteMessage" => "file name did not found, delete operation canceled",
             "deleteStatus" => false);
     }
 
-
 }
-
-
-
-
 
 ?>
